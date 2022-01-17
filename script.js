e96800d5550f0e86740337a33827541cdebce498
Steven Lance <44912347+stevenjlance@users.noreply.github.com>
2022-01-17 16:50:24 +0000

    Solutions added

--- script.js
+++ script.js
@@ -24,19 +24,25 @@ function draw() {
 	rect(0, 380, width, 20)
 	image(ball, ballInfo.x, ballInfo.y, ballInfo.width, ballInfo.height)
 	// Get the ball to accelearte as it falls
-	
-	
+	ballInfo.speedY += ballInfo.acceleration
+	ballInfo.y += ballInfo.speedY
 
 	// LEVEL 1: Stop the ball when it hits the group
-	
-	
-
-
+	if(ballInfo.y > height - 40){
+		ballInfo.speedY = 0
+		ballInfo.acceleration = 0
+		ballInfo.y = height - 40
+		// console.log("running")
+	}
 
 	// LEVEL 2: Get the ball to bounce when it hits the ground
-	
+	if (ballInfo.y > height - 40) {
+    // A little dampening when hitting the bottom
+    ballInfo.speedY *= -0.6;
+    ballInfo.y = height - 40;
+  }
 	
 
-	
+
 	
 }
